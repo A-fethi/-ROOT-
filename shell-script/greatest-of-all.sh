85841e41e0ebfb7f019a6a2c7aaae95aa432862d
A-fethi <fethiabderrahmane1@gmail.com>
2026-03-11 13:50:34 +0000

    Add greatest-of-all exercise

---
+++ shell-script/greatest-of-all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+max=0
+
+for i in {1..10}
+do
+	echo -n "Enter a number: "
+	read num
+
+	if [[ ! "$num" =~ ^[0-9]+$ ]]; then
+        	echo "ERROR: Invalid input only positive numerical characters are allowed"
+        	exit 1
+    	fi
+
+    	if [ "$num" -gt 10000 ]; then
+        	echo "ERROR: The number entered is too large"
+        	exit 1
+    	fi
+
+    	if [ "$num" -gt "$max" ]; then
+        	max=$num
+    	fi
+done
+echo "The largest number is $max"
